b0da85fd36517b8cee46958f654b5f3ef83957c4
Sainath <your-email@gmail.com>
2026-07-07 20:18:14 +0530

    Update project

--- routes/admin.js
+++ routes/admin.js
@@ -9,10 +9,11 @@ var session=require('express-session');
 
 // mysql connect
 var conn=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Root",
-    database:"soulmedic"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 router.use(session({
